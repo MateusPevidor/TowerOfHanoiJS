12ce3685c78ec14dfc9bb6628441feb47cf83ada
Floaddy <mateus-pevidor@hotmail.com>
2020-01-19 16:08:57 -0300

    Configurations menu

--- Menu.js
+++ Menu.js
@@ -1,15 +1,57 @@
+let configButton;
+let configModal;
+let configCancelButton;
+let configApplyButton;
+
+let config_initialState = {
+  blind: false
+};
+
 document.addEventListener('DOMContentLoaded', () => {
-  const configButton = document.getElementById('config-button');
-  const configModal = document.getElementById('config-modal');
-  const configCancelButton = document.getElementById('config-cancelButton');
+  configButton = document.getElementById('config-button');
+  configModal = document.getElementById('config-modal');
+  configCancelButton = document.getElementById('config-cancelButton');
+  configApplyButton = document.getElementById('config-saveButton');
 
   configButton.addEventListener('click', () => {
-    configModal.style.display = 'flex';
-    configButton.style.display = 'none';
+    setInitialState();
+    toggleModal('config');
   });
 
   configCancelButton.addEventListener('click', () => {
-    configModal.style.display = 'none';
-    configButton.style.display = 'inline';
+    restoreInitialState();
+    toggleModal('config');
+  });
+
+  configApplyButton.addEventListener('click', () => {
+    toggleModal('config');
+    applyConfigs();
   });
 });
+
+function toggleModal(type) {
+  switch(type) {
+    case 'config': {
+      if (configModal.style.display == 'none' || configModal.style.display == '') configModal.style.display = 'flex'
+      else configModal.style.display = 'none';
+    
+      if (configButton.style.display == 'inline' || configButton.style.display == '') configButton.style.display = 'none'
+      else configButton.style.display = 'inline';
+      
+      break;
+    }
+  }
+}
+
+function setInitialState() {
+  config_initialState.blind = document.getElementById('blind-checkbox').checked;
+}
+
+function restoreInitialState() {
+  document.getElementById('blind-checkbox').checked = config_initialState.blind;
+}
+
+function applyConfigs() {
+  if (document.getElementById('blind-checkbox').checked) blind = true;
+  else blind = false;
+}
